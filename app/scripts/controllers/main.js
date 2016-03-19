af99a26d843e92797ca283b134f54a9c1fb303c6
rajat1505 <rajatagarwal1505@gmail.com>
2016-03-20 00:07:28 +0530

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name practiceAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the practiceAngularApp
+ */
+angular.module('practiceAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
